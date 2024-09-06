2e8fc3b97f5a354895a3387c940fad8bcf7d32b0
Jarid Prince <109533655+idabblewith@users.noreply.github.com>
2024-09-06 09:03:06 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/env bash
 
 # Define the license template with placeholders for environment variables
-LICENSE_TEMPLATE=$(cat <<EOF
-<license id="${PRINCE_LICENSE_ID}">
-    <name>Server License</name>
-    <vendor>YesLogic</vendor>
-    <product>Prince</product>
-    <version>15</version>
-    <end-user>Government of Western Australia</end-user>
-    <date>2024-05-31</date>
-    <signature>${PRINCE_LICENSE_SIGNATURE}</signature>
-    <option id="upgrades">20250531</option>
-    <option id="expires">20250531</option>
-</license>
-EOF
-)
+# LICENSE_TEMPLATE=$(cat <<EOF
+# <license id="${PRINCE_LICENSE_ID}">
+#     <name>Server License</name>
+#     <vendor>YesLogic</vendor>
+#     <product>Prince</product>
+#     <version>15</version>
+#     <end-user>Government of Western Australia</end-user>
+#     <date>2024-05-31</date>
+#     <signature>${PRINCE_LICENSE_SIGNATURE}</signature>
+#     <option id="upgrades">20250531</option>
+#     <option id="expires">20250531</option>
+# </license>
+# EOF
+# )
 
 # Output the license template to license.dat
-echo "$LICENSE_TEMPLATE" > /usr/lib/prince/license/license.dat
+# echo "$LICENSE_TEMPLATE" > /usr/lib/prince/license/license.dat
+ln -s /usr/src/app/backend/files/license.dat /usr/lib/prince/license/license.dat
+ 
 
 # Launch backend (moved from Dockerfile to run after securely setting Prince license)
 echo "Launching gunicorn..."
